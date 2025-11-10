bb83463ea9dff702af909fccf71b4d69e5d95a85
Kalyani Nawale <kalyaninawale036@gmail.com>
2025-11-10 17:58:41 +0530

    first commit

--- ass1_2.py
+++ ass1_2.py
@@ -1,4 +1,4 @@
-
+# Input three numbers from the user #
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 num3 = float(input("Enter the third number: "))
@@ -10,4 +10,4 @@ elif num2 >= num1 and num2 >= num3:
 else:
     greatest = num3
 
-print("The greatest number among", num1, ",", num2, "and", num3, "is", greatest)
+print(f"The greatest number among {num1}, {num2}, and {num3} is {greatest}")
