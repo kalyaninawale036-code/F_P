5d5f568c660160f711b8b1a6545f3d7e75a698d5
Kalyani Nawale <kalyaninawale036@gmail.com>
2025-11-11 18:00:20 +0530

    first commit

---
+++ ass1_9.py
@@ -0,0 +1,7 @@
+total_sum = 0  # Initialize a variable to store the sum
+
+# Loop through numbers from 2 to 50, incrementing by 2 to get only even numbers
+for number in range(2, 51, 2):
+    total_sum += number  
+
+print("The sum of all even numbers between 1 and 50 is:", total_sum)
