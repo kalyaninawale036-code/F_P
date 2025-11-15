5639882d2817628bf2c0f6ac97107fcb3e6d0f0a
Kalyani Nawale <kalyaninawale036@gmail.com>
2025-11-15 17:49:54 +0530

    first commit

---
+++ ass2_8.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter the first string
+string1 = input("Enter the first string: ")
+
+# Prompt the user to enter the second string
+string2 = input("Enter the second string: ")
+
+# Concatenate the two strings with a space in between
+concatenated_string = string1 + " " + string2
+
+# Display the concatenated string
+print("The concatenated string is:", concatenated_string)
